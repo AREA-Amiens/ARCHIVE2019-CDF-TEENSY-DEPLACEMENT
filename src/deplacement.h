1ffa5004e2fdc649ab6673315c3786bb4c1982d8
albanlefe <albanlefebvre1775@gmail.com>
2019-01-26 08:15:23 +0100

    commande des deux moteur + transmiton de troi trame (debut)

--- src/deplacement.h
+++ src/deplacement.h
@@ -2,14 +2,21 @@
 #include <Wire.h>         //liaison I2C pour les cartes
 #include <FastCRC.h>      //gestion d'erreur sur bus I2C
 #include <AccelStepper.h> //un moteur pas a pas
+        //AccelStepper motor_left(1, step_left, dir_left); definie la comme avec le diver
+        //
 #include <MultiStepper.h> //getion simultaner de moteur pas a pas limieter
 
 #define my_adr 10         //mon adresse bus I2C
 
-#define reset_left  13
-#define sleep_left  15
-#define step_left   0
-#define dir_left    1
+#define reset_G   13 // pin du moteur gauche
+#define sleep_G   15
+#define step_G    0
+#define dir_G     1
+
+#define reset_D   9// pin du moteur droit
+#define sleep_D   10
+#define step_D    11
+#define dir_D     12
 
 
 #define acceleration  100
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,63 @@
 #include "deplacement.h"
 
 //deplacement scotch
-AccelStepper motor_left(1, step_left, dir_left);
+AccelStepper motor_G(1, step_G, dir_G);
+AccelStepper motor_D(1, step_D, dir_D);
 
-
+int t[3],com=0;
+int vmd,vmg;
 void setup() {
-//  Wire.begin(my_adr);
-//  Serial.begin(9600);
-//  Wire.onReceive(receiveEvent);
-//  Wire.onRequest(requestEvent);
-//  Serial.println("coucou");
+  char i;
+  Wire.begin(my_adr);
+  Serial.begin(9600);
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+  Serial.println("coucou");
 
-  pinMode(reset_left, OUTPUT);
-  digitalWrite(reset_left, HIGH);
-  pinMode(sleep_left, OUTPUT);
-  digitalWrite(sleep_left, HIGH);
+  //initialisation du moteur gauche
+  pinMode(reset_G, OUTPUT);    //le resete se fait a l'aita bas
+  digitalWrite(reset_G, HIGH);
+  pinMode(sleep_G, OUTPUT);    //le sleep se met a l'aita bas poru une carte fonctionelle
+  digitalWrite(sleep_G, HIGH);
 
-  motor_left.setSpeed(100);
-  motor_left.setAcceleration(100);
-  motor_left.moveTo(32000);
+  motor_G.setSpeed(100);
+  motor_G.setAcceleration(100);
+
+
+  //initialisation du moteur doit
+  pinMode(reset_D, OUTPUT);    //le resete se fait a l'aita bas
+  digitalWrite(reset_D, HIGH);
+  pinMode(sleep_D, OUTPUT);    //le sleep se met a l'aita bas poru une carte fonctionelle
+  digitalWrite(sleep_D, HIGH);
+
+  motor_D.setSpeed(100);
+  motor_D.setAcceleration(100);
+
+  motor_G.moveTo(13263);
+  motor_D.moveTo(13263);
+
+
+  for(i=0;i<3;i++)t[i]=0;
 }
-
+//motor_G.moveTo(-500);
+//motor_D.moveTo(500);
+//motor_G.run();
+//motor_D.run();
 void loop() {
-  motor_left.run();
+  char i;
+  motor_D.run();
+  motor_G.run();
+
+
+  for(i=0;i<3;i++)Serial.println(t[i]);
+  delay(10000);
 }
 void receiveEvent(int howMany){
-  Serial.println("receivedl");
-  Serial.println(Wire.read());
+  if(com>3)com=0;
+  t[com]=Wire.read();
+  com++;
+  //char res;
+  //res=Wire.read();
 }
 void requestEvent(){
   Wire.write('a');
